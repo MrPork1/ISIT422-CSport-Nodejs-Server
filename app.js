7f66e7fda6986e1f4c91719289ed05c3d09ad89d
Hnicoulin <82177528+Hnicoulin@users.noreply.github.com>
2022-10-11 21:44:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ async function main(){
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-  const uri = "mongodb+srv://<iluvjuntae>:<somuch>@haleynisit420.cj3rn.mongodb.net/?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://iluvjuntae:somuch@haleynisit420.cj3rn.mongodb.net/?retryWrites=true&w=majority";
 
 
   const client = new MongoClient(uri);
